4c0bbd29cea76729c0e157493d35058313cf840d
canakolo <kana.04321@gmail.com>
2020-07-14 13:17:52 +0900

    教師：マイページ確認画面　クエリが失敗する要因を修正
    modified:   teachers/mypage_confirm.php

--- allergin_upload/teachers/mypage_confirm.php
+++ allergin_upload/teachers/mypage_confirm.php
@@ -4,6 +4,15 @@
     debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「献立登録確認ページ');
     debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
 
+    /*$_POSTに引き渡された値をセッションに保存*/
+    $_SESSION['surve_date1'] = $_POST['surve_date1'];
+    $_SESSION['menu1'] = $_POST['menu1'];
+    $_SESSION['menu1_allergin1'] = $_POST['menu1_allergin1'];
+    $_SESSION['menu1_allergin2'] = $_POST['menu1_allergin2'];
+    $_SESSION['menu1_allergin3'] = $_POST['menu1_allergin3'];
+    $_SESSION['menu1_allergin4'] = $_POST['menu1_allergin4'];
+    $_SESSION['menu1_allergin5'] = $_POST['menu1_allergin5'];
+
     $postFoodName = array(
       '0' => '',
       '1' => '卵',
@@ -24,14 +33,14 @@
 
     if(!empty($_POST['register'])){
         debug('登録ボタンが押されました。');
-        if(!empty($_POST)){
-            $surve_date1 = $_POST['surve_date1'];
-            $menu1 = $_POST['menu1'];
-            $menu1_allergin1 = $_POST['menu1_allergin1'];
-            $menu1_allergin2 = $_POST['menu1_allergin2'];
-            $menu1_allergin3 = $_POST['menu1_allergin3'];
-            $menu1_allergin4 = $_POST['menu1_allergin4'];
-            $menu1_allergin5 = $_POST['menu1_allergin5'];
+
+        $surve_date1 = $_SESSION['surve_date1'];
+        $menu1 = $_SESSION['menu1'];
+        $menu1_allergin1 = $_SESSION['menu1_allergin1'];
+        $menu1_allergin2 = $_SESSION['menu1_allergin2'];
+        $menu1_allergin3 = $_SESSION['menu1_allergin3'];
+        $menu1_allergin4 = $_SESSION['menu1_allergin4'];
+        $menu1_allergin5 = $_SESSION['menu1_allergin5'];
 
             if(empty($err_msg)){
              debug('バリデーションOKです');
@@ -57,7 +66,6 @@
               debug("エラーがあります");
             }
         }
-    }
 ?>
 
 <!DOCTYPE html>
@@ -72,42 +80,43 @@
     <body>
         <?php require('header.php'); ?>
         <section id="main">
-            <div id="contents" class="site-width normal-background">
-                <label class="">
-                    入力した内容を確認し、登録してください。
-                </label>
-                <table class="menu1">
-                    <tr>
-                      <td><?php echo $_POST['surve_date1']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>献立１の名前：</td>
-                        <td><?php echo $_POST['menu1']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>アレルギー成分１：</td>
-                        <td><?php echo $postFoodName[$_POST['menu1_allergin1']]; ?></td>
-                    </tr>
-                    <tr>
-                        <td>アレルギー成分２：</td>
-                        <td><?php echo $postFoodName[$_POST['menu1_allergin2']]; ?></td>
-                    </tr>
-                    <tr>
-                        <td>アレルギー成分３：</td>
-                        <td><?php echo $postFoodName[$_POST['menu1_allergin3']]; ?></td>
-                    </tr>
-                    <tr>
-                        <td>アレルギー成分４：</td>
-                        <td><?php echo $postFoodName[$_POST['menu1_allergin4']]; ?></td>
-                    </tr>
-                    <tr>
-                        <td>アレルギー成分５：</td>
-                        <td><?php echo $postFoodName[$_POST['menu1_allergin5']]; ?></td>
-                    </tr>
-                </table>
-                <div class="btn-container">
-                    <input type="submit" name="register" class="btn btn-mid" value="登録する">
-                </div>
+            <form method="post">
+                <div id="contents" class="site-width normal-background">
+                    <label class="">
+                        入力した内容を確認し、登録してください。
+                    </label>
+                    <table class="menu1">
+                        <tr>
+                          <td><input type="hidden" name="surve_date1" value="<?php echo $_SESSION['surve_date1']; ?>"><?php echo $_SESSION['surve_date1']; ?></td>
+                        </tr>
+                        <tr>
+                            <td>献立１の名前：</td>
+                            <td><input type="hidden" name="menu1" value="<?php echo $_SESSION['menu1']; ?>"><?php echo $_SESSION['menu1']; ?></td>
+                        </tr>
+                        <tr>
+                            <td>アレルギー成分１：</td>
+                            <td><input type="hidden" name="menu1_allergin1" value="<?php echo $_SESSION['menu1_allergin1']; ?>"><?php echo $postFoodName[$_SESSION['menu1_allergin1']]; ?></td>
+                        </tr>
+                            <td>アレルギー成分２：</td>
+                            <td><input type="hidden" name="menu1_allergin2" value="<?php echo $_SESSION['menu1_allergin2']; ?>"><?php echo $postFoodName[$_SESSION['menu1_allergin2']]; ?></td>
+                        </tr>
+                        <tr>
+                            <td>アレルギー成分３：</td>
+                            <td><input type="hidden" name="menu1_allergin3" value="<?php echo $_SESSION['menu1_allergin3']; ?>"><?php echo $postFoodName[$_SESSION['menu1_allergin3']]; ?></td>
+                        </tr>
+                        <tr>
+                            <td>アレルギー成分４：</td>
+                            <td><input type="hidden" name="menu1_allergin4" value="<?php echo $_SESSION['menu1_allergin4']; ?>"><?php echo $postFoodName[$_SESSION['menu1_allergin4']]; ?></td>
+                        </tr>
+                        <tr>
+                            <td>アレルギー成分５：</td>
+                            <td><input type="hidden" name="menu1_allergin5" value="<?php echo $_SESSION['menu1_allergin5']; ?>"><?php echo $postFoodName[$_SESSION['menu1_allergin5']]; ?></td>
+                        </tr>
+                    </table>
+                    <div class="btn-container">
+                        <input type="submit" name="register" class="btn btn-mid" value="登録する">
+                    </div>
+                </form>
                 <div class="btn-container">
                     <input type="button" onclick="history.back()" value="戻る">
                 </div>
